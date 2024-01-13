1d3d430e046f527cf8a3809fef0709e87d0a3e78
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-13 18:55:00 -0300

    github test and build makefile

---
+++ Classes/Class03/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Lista {
+    int node;
+    Lista *next;
+}
+
+int main() {
+    Lista *l;
+    int x = 20;
+
+    l->inserir(x);
+    // ...
+}
+
+void inserir (int x){
+    l->arranjo[...] = x;
+    ...
+}
+
+void inserir (int x) {
+    Celula *c = criar_celula(x);
+    l->ultimo = c;
+}
+
+Para: "Chefe DCC" <chefe@dcc.ufmg.br>
+Cc: chefia@dcc.ufmg.br
