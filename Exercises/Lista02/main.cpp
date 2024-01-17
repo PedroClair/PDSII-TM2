3493c92904c5f2184feadf576ba4e797094b7c5c
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-17 14:13:39 -0300

    .gitignore is now working

--- Exercises/Lista02/main.cpp
+++ Exercises/Lista02/main.cpp
@@ -6,3 +6,26 @@ int main (){
     return 0;
 }
     
+/*
+c_cpp_prperties.json
+{
+    "configurations": [
+        {
+            "name": "Win32",
+            "includePath": [
+                "${workspaceFolder}/**"
+            ],
+            "defines": [
+                "_DEBUG",
+                "UNICODE",
+                "_UNICODE"
+            ],
+            "compilerPath": "C:/MinGW/bin/g++.exe",
+            "cStandard": "c17",
+            "cppStandard": "c++17",
+            "intelliSenseMode": "windows-gcc-x64"
+        }
+    ],
+    "version": 4
+}
+*/
