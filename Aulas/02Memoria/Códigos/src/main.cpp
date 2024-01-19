af2750d49a51c8fb1caf990105509ecf11b5ef66
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-19 15:11:39 -0300

    Codigos relacionados a ponteiros

--- Aulas/02Memoria/Códigos/src/main.cpp
+++ Aulas/02Memoria/Códigos/src/main.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
 
+using namespace std;
+
+void exemplo3bom();
+void exemplo3ruim();
+
+
+int main() {
+    exemplo3bom();
+    exemplo3ruim();
+    return 0;
+}
+
+void exemplo3bom(){
+    int *a = nullptr;
+    int b = 10;
+    a = new int;
+    *a = 20;
+    delete a;
+    a = &b;
+    *a = 30;
+    cout << "Bom => " << "*a: " << *a << " b: " << b << endl;
+}
+
+void exemplo3ruim(){
+    int *a, b;
+    b = 10;
+    a = new int;
+    *a = 20;
+    a = &b;
+    *a = 30;
+    cout << "Ruim => " << "*a: " << *a << " b: " << b << endl;
+}
+
+void exemplo2 (){
+    int *p1, *p2;
+    p1 = new int;
+    *p1 = 42;
+    p2 = p1;
+    cout << "*p1 == " << *p1 << endl;
+    cout << "*p2 == " << *p2 << endl;
+    *p2 = 53;
+    cout << "*p1 == " << *p1 << endl;
+    cout << "*p2 == " << *p2 << endl;
+    p1 = new int;
+    *p1 = 88;
+    cout << "*p1 == " << *p1 << endl;
+    cout << "*p2 == " << *p2 << endl;
+}
+
+
 double multiplyByTwo(double input) {
     double twice = input * 2.0;
     return twice;
@@ -13,11 +63,4 @@ void chamaMultipyByTwo (){
         myList[i] = multiplyByTwo(myList[i]);
 
     std::cout << multiplyByTwo(salary) << std::endl;
-}
-
-int main() {
-    int i = 10;
-    int *ponteiro = &i;
-    int **ppp = &ponteiro;
-    return 0;
 }
