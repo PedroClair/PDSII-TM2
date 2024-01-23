64488637c83551bf58c8b9a081b0766a869d7c74
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-23 16:20:10 -0300

    Implementação da TAD circunferencia em modulos

--- Aulas/03TADs/main.cpp
+++ Aulas/03TADs/main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+struct TriangRetangulo {
+    float base, altura;
+};
+
+struct TriangRetangulo {
+    float base, hipot, ang;
+};
+
 int main() {
     Circunferencia* circ = new Circunferencia(0, 0, 10);
     cout << circ->calcularArea() << endl;
