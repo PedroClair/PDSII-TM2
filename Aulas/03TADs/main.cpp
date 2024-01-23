7065d3280b20ffd925597f40dd1f7292718d7b7f
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-23 09:48:02 -0300

    Imprementação da TAD Ponto3D

--- Aulas/03TADs/main.cpp
+++ Aulas/03TADs/main.cpp
@@ -1,14 +1,34 @@
+#include <cstdlib>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 struct Ponto3D {
-    double coordenadaX;
-    double coordenadaY;
-    double coordenadaZ;
+    float coordenadaX;
+    float coordenadaY;
+    float coordenadaZ;
+
+    float calcularDistancia (Ponto3D* p2){
+        float dx = p2->coordenadaX;
+        float dy = p2->coordenadaY;
+        float dz = p2->coordenadaZ;
+        return sqrt(dx*dx + dy*dy + dz*dz);
+    }
 };
 
 int main ( ){
-    cout << "TAD de ponto 3d!!" << endl;
+    Ponto3D* p1 = new Ponto3D;
+    p1->coordenadaX = 0;
+    p1->coordenadaY = 0;
+    p1->coordenadaZ = 0;
+
+    Ponto3D* p2 = new Ponto3D;
+    p2->coordenadaX = 5;
+    p2->coordenadaY = 5;
+    p2->coordenadaZ = 5;
+
+    cout << p1->calcularDistancia(p2) << endl;
+
     return 0;
 }
