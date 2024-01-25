26cfe36885e1663b237c815df0513abff2da8732
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-25 11:28:42 -0300

    solved issGeo

--- Projetos/Geometria/src/main.cpp
+++ Projetos/Geometria/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../include/ponto.hpp"
+#include "ponto.hpp"
 
 int main (){
     Ponto origem = Ponto(0.0, 0.0);
--- Projetos/Geometria/src/ponto.cpp
+++ Projetos/Geometria/src/ponto.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../include/ponto.hpp"
+#include "ponto.hpp"
 
 Ponto::Ponto(double x, double y) {
   coordenadaHorizontalX = x;
