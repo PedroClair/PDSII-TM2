512c7c86e0f6ca7d42e68170c7ee34830603b869
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-30 17:35:48 -0300

    dificult remove Implementation

--- Aulas/05ListasEArvores/src/lista/List.cpp
+++ Aulas/05ListasEArvores/src/lista/List.cpp
@@ -1,7 +1,6 @@
 #include "List.hpp"
 
 void List::insertNode(int data) {
-    
     Node* aux = new Node;
     aux->data = data;
     aux->next = nullptr;
@@ -12,5 +11,39 @@ void List::insertNode(int data) {
     } else {
         tail->next = aux;
         tail = aux;
+    }
+}
+
+void List::removeNode(int data) {
+    Node *current = head;
+    Node *previous = nullptr;
+    while (current != nullptr) {
+        if (current->data == data) {
+            if (previous == nullptr) { // HEAD
+                head = current->next;
+            } else if (current->next == nullptr) { //TAIL
+                previous->next = nullptr;
+                tail = previous;
+            } else {
+                previous->next = current->next;
+            }
+            delete current;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+}
+
+void List::display(){
+    if (head) {
+        cout << head->data << endl;
+        Node* aux = head;
+        while (aux->next != nullptr){
+            aux = aux->next;
+            cout << aux->data << endl;
+        }   
+    } else {
+        cout << "Lista vazia" << endl;
     }
 }
--- Aulas/05ListasEArvores/src/main.cpp
+++ Aulas/05ListasEArvores/src/main.cpp
@@ -4,5 +4,8 @@ int main() {
     List lista;
     lista.insertNode(111);
     lista.insertNode(222);
+    lista.insertNode(333);
+    lista.removeNode(222);
+    lista.display();
     return 0;
 }
