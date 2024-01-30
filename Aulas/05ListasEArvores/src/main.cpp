1d451b8ab29b45fd98009b7c4b07689b9c9017bb
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-01-30 17:53:53 -0300

    dificult remove Implementation

--- Aulas/05ListasEArvores/src/main.cpp
+++ Aulas/05ListasEArvores/src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     lista.insertNode(111);
     lista.insertNode(222);
     lista.insertNode(333);
-    lista.removeNode(222);
+    lista.removeNode(0);
     lista.display();
     return 0;
 }
