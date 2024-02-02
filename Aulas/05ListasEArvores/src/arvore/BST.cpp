a8a09207d0724295d73cb4ce02ac9f99a24d3dbf
pedrosrk <pedro.metalurgia@gmail.com>
2024-02-02 19:15:12 -0300

    tree insertion

--- Aulas/05ListasEArvores/src/arvore/BST.cpp
+++ Aulas/05ListasEArvores/src/arvore/BST.cpp
@@ -1,5 +1,39 @@
 #include "BST.hpp"
 
+NodeT* createNode (int data){
+    NodeT *aux = new NodeT;
+    aux->data = data;
+    aux->esq = nullptr;
+    aux->dir = nullptr;
+    return aux;
+}
+
+void BST::insertNode(int data){
+    if (root == nullptr){
+        root = createNode(data);
+    } else {
+        insertNodeHelper(root, data);
+    }
+}
+
+void BST::insertNodeHelper(NodeT* root, int data){
+    if (data < root->data){
+        if (root->esq == nullptr){
+            root->esq = createNode(data);
+        } else {
+            insertNodeHelper(root->esq, data);
+        }
+    } else if (data > root->data) {
+        if (root->dir == nullptr){
+            root->dir = createNode(data);
+        } else {
+            insertNodeHelper(root->dir, data);
+        }
+    } else {
+        cout << "Dado existente!!" << endl;
+    }
+}
+
 void BST::display(){
     cout << "Mostra a BST!!!" << endl;
 }
