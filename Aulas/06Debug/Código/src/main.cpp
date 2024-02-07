bd1575eea7340fdf6bcde8b5dde472e44e145b26
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-02-07 15:24:51 -0300

    practice debug with gdb

--- Aulas/06Debug/Código/src/main.cpp
+++ Aulas/06Debug/Código/src/main.cpp
@@ -1,7 +1,22 @@
-#include "ola.hpp"
+#include <iostream>
+using namespace std;
+
+int findMax(int *array, int len) {
+  int max = array[0];
+  for(int i=1; i < len; i++) {
+    if(max < array[i]) {
+      max = array[i];
+    }
+  }
+  return max;
+}
 
 int main() {
-	hello();
-  return 0;
+	int arr[5] = {60, 17, 21, 44, 2};
+
+	int max = findMax(arr, 5);
+	cout << "Valor maximo e: " << max;
+
+	return 0;
 }
 
