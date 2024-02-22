73b6b1e428eef03f549720bab42de884ffcf0ef0
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-02-22 08:55:07 -0300

    reestart repository

--- Projeto/include/intro.hpp
+++ Projeto/include/intro.hpp
@@ -1,13 +1,15 @@
-#ifndef L1CAP1_H
-#define L1CAP1_H
+#ifndef INTRO_H
+#define INTRO_H
 
 void lista1Cap1 ();
 
 #endif
 
-/* Cap. 1 (Projeto de Programação 6). Um empregado recebe R$ 16,78 por horas regulares trabalhadas em uma semana. 
+/* 
+Cap. 1 (Projeto de Programação 6). Um empregado recebe R$ 16,78 por horas regulares trabalhadas em uma semana. 
 Se esse empregado fizer hora extra, deve receber essa mesma taxa multiplicada por 1,5. Do pagamento bruto do empregado, 
 6% são retidos pela Previdência Social, 14%, pelo Imposto de Renda Federal, 5%, por impostos estaduais, e R$ 10 por semana, para o Sindicato. 
 Se o empregado tiver três ou mais dependentes, um adicional de R$ 35 é retido para cobrir o custo extra do seguro de saúde. 
 Escreva um programa que leia o número de horas trabalhadas em uma semana e o número de dependentes como entrada e apresente como saída o pagamento bruto do empregado, 
-o valor de cada imposto retido e o salário líquido por semana. */
+o valor de cada imposto retido e o salário líquido por semana. 
+*/
