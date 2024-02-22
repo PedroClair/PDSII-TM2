6f4b74917e8caa20944f2c1e8a1d0a017d91f327
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-02-22 10:14:13 -0300

    makefile config

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include "ponto3d.hpp"
 
 int main() {
-    std::cout << "Lets code!" << std::endl;
-    Ponto3D p;
-    p.coordenadaX = 0;
-    p.coordenadaY = 0;
-    p.coordenadaZ = 0;
-    Ponto3D *ptr = new Ponto3D();
-    ptr->coordenadaX = 2;
-    ptr->coordenadaY = 2;
-    ptr->coordenadaZ = 2;
-    std::cout << p.calcularDistancia(ptr) << std::endl;
+    std::cout << "Lets code oop!" << std::endl;
     return 0;
 }
