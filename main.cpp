5eb4509119d57bb48da0f4b5aaf3f65364cf0896
PedroClair <pedro.clair@aluno.ufop.edu.br>
2024-02-22 11:49:24 -0300

    poo destrutores

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 
 class TestObject {
     int atributo;
-
     public:
     TestObject (int valor) : atributo(valor) {}
     ~TestObject () { std::cout << "~TestObject " << atributo << std::endl; }
+    void testaDestrutores(){
+        TestObject o1(1);
+        TestObject* o2 = new TestObject(2);
+        delete (o2);
+    }
 };
 
-
 int main() {
-   TestObject o1(1);
-   TestObject* o2 = new TestObject(2);
-
-   delete (o2);
-   return 0;
+    std::cout << "Antes" << std::endl;
+    int i = 0;
+    while (i < 5){
+        TestObject o(i);
+        i++;
+    }
+    std::cout << "Depois" << std::endl;
+    return 0;
 }
