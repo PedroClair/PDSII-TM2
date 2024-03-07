e24f8ed90e4b94c1a88ca355eff69b564d0dbe00
pedrosrk <pedro.metalurgia@gmail.com>
2024-03-07 13:26:00 -0300

    Ensaio da primeira aula PDS2

--- ensaio.cpp
+++ ensaio.cpp
@@ -1,8 +1,47 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main (){
-    cout << "Baum!" << endl;        
+int main () {
+    ifstream in("entrada.txt", fstream::in);
+    ofstream out("saída.txt", fstream::out);
+    string line;
+    while (getline(in, line)){
+        cout << "*" << line << "*" << endl;
+        out << "[" << line << "]" << endl;
+    }
     return 0;
 }
+
+
+
+
+/*
+int main() {
+	int x;
+
+	do {
+		cin >> x;
+		cout << "[" << x << "]" << endl;
+	} while (x!=0);
+
+	return 0;
+}
+
+
+int main() {
+  string nome;
+  int idade;
+  cout << "Digite o seu nome: ";
+  cin >> nome;
+  cout << "Digite sua idade: ";
+  cin >> idade;
+  cout << "Ola " << nome;
+  cout << " voce tem " << idade; 
+  cout << " anos." << endl;
+  return 0;
+}
+*/
+
